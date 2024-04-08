81f44a63daa760356372df9b9f5c9f7d5aff08f5
VecTor <lsh961101@naver.com>
2024-04-08 19:00:45 +0900

    321

---
+++ RiskofRain2/Scene1.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include "Scene1.h"
+
+
+
+Scene1::Scene1()
+{
+
+}
+
+Scene1::~Scene1()
+{
+
+}
+
+void Scene1::Init()
+{
+
+}
+
+void Scene1::Release()
+{
+}
+
+void Scene1::Update()
+{
+}
+
+void Scene1::LateUpdate()
+{
+
+}
+
+void Scene1::PreRender()
+{
+}   
+
+void Scene1::Render()
+{
+}
+
+void Scene1::ResizeScreen()
+{
+}
---
+++ RiskofRain2/Scene1.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Scene1 : public Scene
+{
+private:
+
+public:
+    Scene1();
+    ~Scene1();
+
+    // Scene을(를) 통해 상속됨
+    virtual void Init() override;
+    virtual void Release() override;
+    virtual void Update() override;
+    virtual void LateUpdate() override;
+    virtual void PreRender() override;
+    virtual void Render() override;
+    virtual void ResizeScreen() override;
+};
+
